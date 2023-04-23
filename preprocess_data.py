ce7a9552c56ea1a6fa32ded0d86c9b97eb1e04af
Nisrine Kane <34342007+nisrinekane@users.noreply.github.com>
2023-04-23 14:00:43 -0700

    retrain model and evaluate it

--- preprocess_data.py
+++ preprocess_data.py
@@ -8,7 +8,7 @@ data = data[(data['No Finding'] == 1) | (data['Pneumonia'] == 1)]
 # create a new column for labels
 data['Label'] = data.apply(lambda row: 'No Finding' if row['No Finding'] == 1 else 'Pneumonia', axis=1)
 # drop unnecessary columns
-columns_to_drop = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia', 'Pneumothrax', 'Pneumoperitoneum', 'Pneumomediastinum', 'Subcutaneous Emphysema', 'Tortuous Aorta', 'Calcification of the Aorta', 'No Finding', 'subject_id']
+columns_to_drop = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema', 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax', 'Pneumoperitoneum', 'Pneumomediastinum', 'Subcutaneous Emphysema', 'Tortuous Aorta', 'Calcification of the Aorta', 'No Finding', 'subject_id']
 # use id for image name
 image_column = 'id'
 
@@ -20,5 +20,3 @@ train_data, val_data = train_test_split(train_data, test_size=0.25, random_state
 train_data.to_csv('train.csv', index=False)
 val_data.to_csv('val.csv', index=False)
 test_data.to_csv('test.csv', index=False)
-
-
--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ from data_utils import generate_data_train
 from PIL import Image
 
 # defining early stopping and model checkpoint
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 model_checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, verbose=1)
 
 # parameters and paths:
@@ -25,20 +25,22 @@ train_image_directory = 'CXR8/images/images'
 val_image_directory = 'CXR8/images/images'
 image_size = (224, 224)
 batch_size = 32
-epochs = 15
+epochs = 10
 
 # function for data augmentation to diversify data and prevent overfitting
-def create_datagen():
+def create_datagen(rotation_range=0, zoom_range=0, width_shift_range=0, height_shift_range=0, shear_range=0, horizontal_flip=False, fill_mode="nearest"):
     return ImageDataGenerator(
-        rotation_range=20,
-        zoom_range=0.15,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.15,
-        horizontal_flip=True,
-        fill_mode="nearest"
+        rotation_range=rotation_range,
+        zoom_range=zoom_range,
+        width_shift_range=width_shift_range,
+        height_shift_range=height_shift_range,
+        shear_range=shear_range,
+        horizontal_flip=horizontal_flip,
+        fill_mode=fill_mode
     )
 
+train_datagen = create_datagen(rotation_range=20, zoom_range=0.15, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15, horizontal_flip=True)
+val_datagen = create_datagen()
 
 # create CNN model using ResNet50 as base model
 base_model = ResNet50(weights='imagenet', include_top=False)
@@ -55,12 +57,8 @@ for layer in base_model.layers:
 model.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
 # train model 
-train_datagen = create_datagen()
-val_datagen = create_datagen()
 train_generator = generate_data_train(train_datagen, train_csv, train_image_directory, image_size, batch_size)
 val_generator = generate_data_train(val_datagen, val_csv, val_image_directory, image_size, batch_size)
-
-
 
 # fit model 
 model.fit(
@@ -70,7 +68,7 @@ model.fit(
     validation_steps=len(val_generator),
     epochs=epochs,
     # prevent overfitting
-    callbacks=[early_stopping, model_checkpoint] 
+    callbacks=[early_stopping, model_checkpoint]
 )
 
 # save trained model with the best weights using early stopping and save best model checkpoint
